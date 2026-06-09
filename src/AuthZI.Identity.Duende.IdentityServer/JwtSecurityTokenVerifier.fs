1d7af2525d8e38c493eb7af263f4869067a14e10
Karen Tazayan <karen.tazayan@outlook.com>
2026-06-09 17:33:03 +0400

    Clarify pipeline dependencies, fix JWT key decoding
    Set explicit dependsOn for Print_SemVer_Number job in pipeline for clarity. Update JwtSecurityTokenVerifier to use Base64UrlEncoder.DecodeBytes for RSA key decoding, improving JWT validation reliability.

--- src/AuthZI.Identity.Duende.IdentityServer/JwtSecurityTokenVerifier.fs
+++ src/AuthZI.Identity.Duende.IdentityServer/JwtSecurityTokenVerifier.fs
@@ -10,8 +10,8 @@ module JwtSecurityTokenVerifier =
     let keys = System.Collections.Generic.List<SecurityKey>()
 
     for webKey in discoveryDocument.Keys do
-      let e = Base64Url.Decode(webKey.E)
-      let n = Base64Url.Decode(webKey.N)
+      let e = Base64UrlEncoder.DecodeBytes(webKey.E)
+      let n = Base64UrlEncoder.DecodeBytes(webKey.N)
       let rsaParameters = new RSAParameters(Exponent = e, Modulus = n)
       let key = RsaSecurityKey(rsaParameters)
       keys.Add key
